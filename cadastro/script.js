178d02640bd0f75ad3515aeda80cca656565efa2
mateusgcvargas <yataaa89@gmail.com>
2025-04-24 19:40:22 -0300

    Adicionado funcionalidade de registro de usuário em array

---
+++ cadastro/script.js
@@ -0,0 +1,32 @@
+const cadform = document.getElementById('cadastroform')
+cadform.addEventListener('submit', function (event){
+    event.preventDefault()
+    
+    //variáveis do input da tela de registro
+
+    const reguser = document.getElementById('username').value
+    const regmat = document.getElementById('matricula').value
+    const regpsswd = document.getElementById('password').value
+
+    //simulação de bancos de dados de usuários
+    const usuarios = JSON.parse(localStorage.getItem('usuarios')) || []
+
+    const usuarioExistente = usuarios.find(user => 
+        user.username === reguser && user.password === regpsswd && user.matricula === regmat
+      )
+  
+      if (usuarioExistente) {
+        alert('Este usuário já existe!')
+      } else {
+        usuarios.push(
+          {username: reguser, password: regpsswd, matricula: regmat}
+        )
+        localStorage.setItem('usuarios', JSON.stringify(usuarios))
+        alert("Cadastro realizado com sucesso!")
+        window.location.href = "projetobib\login\index.html"
+      }
+      console.log(usuarios)
+})
+
+
+
