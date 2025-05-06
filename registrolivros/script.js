6c2432d2696df4471115682722536f501ce0c643
MANUELA KLASER BERGGUIST <emanuelklaser@gmail.com>
2025-05-06 19:33:51 -0300

    no html da pagina de login, foi retirado o href com o catalogo, juntamente com a listra verde que estava na mesma linha de pixel que a href

---
+++ registrolivros/script.js
@@ -0,0 +1,29 @@
+livros = JSON.parse(localStorage.getItem('livros')) || [];
+
+function registrarLivro(){
+    const formreg = document.getElementById('formreg').value
+    const formnome = document.getElementById('campo1-nome').value
+    const formaut = document.getElementById('campo2-autor').value
+    const formano = document.getElementById('campo1-ano').value
+    const formgen = document.getElementById('campo2-genero').value
+
+    if (!formnome || !formaut || !formano || !formgen){
+        alert("Todos os campos precisam estar preenchidos.")
+        return
+    }
+
+    let livros = JSON.parse(localStorage.getItem('livros')) || []
+
+    const novoLivro = {
+        titulo: formnome,
+        autor: formaut,
+        ano: formano,
+        genero: formgen,
+        disp: true
+    }
+
+    livros.push(novoLivro);
+    localStorage.setItem('livros', JSON.stringify(livros))
+    alert("Livro registrado com sucesso!")    
+    document.getElementById('formreg').reset()
+}
